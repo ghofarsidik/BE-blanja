53c16deabee2840e8a73d2cd011d85f7de97a913
Dimas Ageng Saputro <140221298+dimassagngsptr@users.noreply.github.com>
2024-06-28 17:37:51 +0700

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 	// CORS middleware configuration
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://blanja-kelompok-1.vercel.app",
+		AllowOrigins:     "*",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "*",
 		ExposeHeaders:    "Content-Length",
